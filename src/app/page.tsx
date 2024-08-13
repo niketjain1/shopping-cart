0b69b291ae72dbb450df7b8f9052e2326f87fbab
niketjain1 <niketj2000@gmail.com>
2024-08-13 20:11:01 +0530

    👌 IMPROVE: Added loader on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,20 @@ import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
+        setIsLoading(true);
         const response = await axios.get<Product[]>(
           "https://fakestoreapi.com/products?limit=10"
         );
         setProducts(response.data);
       } catch (error) {
         console.error("Error fetching products:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -26,11 +30,17 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold mb-6 text-gray-800">Product Listing</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="flex min-w-full min-h-full h-full items-center justify-center lg:mt-72">
+          <div className="loader" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
