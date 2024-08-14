efecc0b383142052d942eb9afc41b741eb4ddfeb
niketjain1 <niketj2000@gmail.com>
2024-08-14 21:59:45 +0530

    📦 NEW: Added animation on the add to cart button

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useCart } from "../lib/cartContext";
 import { Product } from "@/types";
+import { ShoppingCartIcon, CubeIcon } from "@heroicons/react/20/solid";
+import { motion } from "framer-motion";
 
 interface AddToCartButtonProps {
   product: Product;
@@ -9,6 +11,7 @@ interface AddToCartButtonProps {
 const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product }) => {
   const { addToCart, updateQuantity, cart, removeFromCart } = useCart();
   const [isAdded, setIsAdded] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   const cartItem = cart.find((item) => item.id === product.id);
   const quantity = cartItem ? cartItem.quantity : 0;
@@ -20,10 +23,14 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product }) => {
   }, [quantity]);
 
   const handleAddToCart = () => {
-    if (!isAdded) {
-      addToCart(product);
-      setIsAdded(true);
-    }
+    setClicked(true);
+    setTimeout(() => {
+      if (!isAdded) {
+        addToCart(product);
+        setIsAdded(true);
+        setClicked(false);
+      }
+    }, 1500);
   };
 
   const handleIncreaseQuantity = () => {
@@ -43,12 +50,37 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product }) => {
   return (
     <div className="flex items-center justify-center mt-auto w-full">
       {!isAdded ? (
-        <button
+        <motion.button
           onClick={handleAddToCart}
-          className="bg-yellow-300 text-black px-4 py-2 rounded hover:bg-yellow-500 transition-colors w-full"
+          className={`cart__button relative w-[200px] h-[62px] rounded-[10px] bg-purple-600 text-white text-lg font-medium overflow-hidden transition duration-300 ease-in-out ${
+            clicked ? "scale-90" : ""
+          }`}
+          whileHover={{ backgroundColor: "hsl(250, 57%, 53%)" }}
+          whileTap={{ scale: 0.9 }}
         >
-          Add to Cart
-        </button>
+          <motion.div
+            className="absolute z-20 top-1/2 left-[-10%] transform -translate-x-1/2 -translate-y-1/2"
+            animate={clicked ? { left: "110%" } : { left: "-10%" }}
+            transition={{ duration: 1.5, ease: "easeInOut" }}
+          >
+            <ShoppingCartIcon className="h-8 w-8" />
+          </motion.div>
+          <motion.span
+            className="absolute left-1/2 z-30 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            initial={{ opacity: 1 }}
+            animate={clicked ? { opacity: 0 } : { opacity: 1 }}
+            transition={{ duration: 1.5, ease: "easeInOut" }}
+          >
+            Add to Cart
+          </motion.span>
+          <motion.span
+            className="absolute left-1/2 z-30 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0"
+            animate={clicked ? { opacity: 1 } : { opacity: 0 }}
+            transition={{ duration: 1.5, ease: "easeInOut" }}
+          >
+            Added
+          </motion.span>
+        </motion.button>
       ) : (
         <div className="flex items-center bg-gray-100 rounded w-full justify-between">
           <button
